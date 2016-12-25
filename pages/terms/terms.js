9b595aef863657ee418248d9ba090671d6c37317
Alexander Kozlov <alxnder.kozlov@gmail.com>
2016-12-25 20:15:11 +0200

    023: Mobile friendly terms page.

--- pages/terms/terms.js
+++ pages/terms/terms.js
@@ -1,9 +1,11 @@
 document.querySelector('.term-nav__list').addEventListener('click', function(e) {
-    if(e.target.parentNode.nodeName === 'LI') {
-        Array.prototype.forEach.call(document.querySelectorAll('.term-nav__item'), function (el) {
-            el.classList.remove('term-nav__item_active');
-        });
+    Array.prototype.forEach.call(document.querySelectorAll('.term-nav__item'), function (el) {
+        el.classList.remove('term-nav__item_active');
+    });
 
-        e.target.parentNode.classList.add('term-nav__item_active');
-    }
+    e.target.parentNode.classList.add('term-nav__item_active');
+});
+
+document.getElementsByClassName('header__burger')[0].addEventListener('click', function() {
+    document.querySelector('.header__list').classList.toggle('header__list_open');
 });
