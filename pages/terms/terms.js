7dba0286a57f42c3a75e53ce2a2ced01ab6f8dd6
Alexander Kozlov <alxnder.kozlov@gmail.com>
2016-12-26 02:49:40 +0200

    029: Little fix for terms page js.

--- pages/terms/terms.js
+++ pages/terms/terms.js
@@ -1,15 +1,46 @@
 document.querySelector('.term-nav__list').addEventListener('click', function(e) {
-    if(e.target.nodeName == "UL" || e.target.nodeName == "LI") {
+
+    // First method.
+    // Looking for invalid targets and terminate event.
+
+    //if(e.target.nodeName == "UL" || e.target.nodeName == "LI") {
+    //    return;
+    //}
+    //
+    //Array.prototype.forEach.call(document.querySelectorAll('.term-nav__item'), function (el) {
+    //    el.classList.remove('term-nav__item_active');
+    //});
+    //
+    //e.target.parentNode.classList.add('term-nav__item_active');
+
+    // Second method.
+    // Looking for closest list item and then do some magic.
+
+    //if(!e.target.closest("li")) {
+    //    return;
+    //}
+    //
+    //Array.prototype.forEach.call(document.querySelectorAll('.term-nav__item'), function (el) {
+    //    el.classList.remove('term-nav__item_active');
+    //});
+    //
+    //e.target.closest("li").classList.add('term-nav__item_active');
+
+    // Third method.
+
+    var target = e.target;
+
+    if(!target.classList.contains('term-nav__link')) {
         return;
     }
 
-    Array.prototype.forEach.call(document.querySelectorAll('.term-nav__item'), function (el) {
-        el.classList.remove('term-nav__item_active');
+    Array.prototype.forEach.call(document.querySelectorAll('.term-nav__link'), function(el) {
+        el.classList.remove('term-nav__link_active');
     });
 
-    e.target.parentNode.classList.add('term-nav__item_active');
+    target.classList.add('term-nav__link_active');
 });
 
 document.getElementsByClassName('header__burger')[0].addEventListener('click', function() {
     document.querySelector('.header__list').classList.toggle('header__list_open');
-});
+}, false);
