4feda3a7784a11077fd799cb5b8353e4bf951e12
Alexander Kozlov <alxnder.kozlov@gmail.com>
2017-01-23 19:02:05 +0200

    Fix: removed alert by console.log.

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -23,7 +23,7 @@ function initMap() {
                 position: results[0].geometry.location
             });
         } else {
-            alert('Geocode was not successful for the following reason: ' + status);
+            console.log('Geocode was not successful for the following reason: ' + status);
         }
     });
 }
